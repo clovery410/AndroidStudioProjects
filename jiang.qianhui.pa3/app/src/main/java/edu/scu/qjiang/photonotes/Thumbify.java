4963871e9fe121100a0991d5ff915d21ddbc6152
clovery <seasoul410@gmail.com>
2016-05-17 07:12:09 -0700

    finish the recycler view

---
+++ jiang.qianhui.pa3/app/src/main/java/edu/scu/qjiang/photonotes/Thumbify.java
@@ -0,0 +1,25 @@
+package edu.scu.qjiang.photonotes;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.ThumbnailUtils;
+
+import java.io.FileOutputStream;
+
+/**
+ * Created by clover on 5/11/16.
+ */
+public class Thumbify {
+    static public void generateThumbnail(String imgFile, String thumbFile) {
+        try {
+            Bitmap picture = BitmapFactory.decodeFile(imgFile);
+            Bitmap resized = ThumbnailUtils.extractThumbnail(picture, 120, 120);
+            FileOutputStream fos = new FileOutputStream(thumbFile);
+            resized.compress(Bitmap.CompressFormat.JPEG, 90, fos);
+            fos.flush();
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
